34bd79e6e6cd1dd997e8685b3d3c16b1b7562197
recyon <recyon@gmail.com>
2020-06-29 16:35:37 +0900

    Updates

---
+++ precache-manifest.60595ed05d4892a22fd662ad15518967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3da4570ef5f1727bb547d1dd72d0a3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "bc29ed7e9aee9f3489db",
+    "url": "/movie_app_2020/static/css/main.898ff26e.chunk.css"
+  },
+  {
+    "revision": "b0f64fb423232732c18c",
+    "url": "/movie_app_2020/static/js/2.229824db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.229824db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc29ed7e9aee9f3489db",
+    "url": "/movie_app_2020/static/js/main.e98af16a.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
